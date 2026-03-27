48ecc3d5962f1c8b9db4b3865d9749d27574510e
MasterOogway1466 <manas.shetty04@gmail.com>
2026-03-27 11:57:30 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,42 @@
+use pathsearch::find_executable_in_path;
+#[allow(unused_imports)]
 use std::io::{self, Write};
-
-const VALID_COMMANDS: [&str; 3] = ["exit", "echo", "type"];
 
 fn main() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
+        let mut input: String = "".to_string();
+        io::stdin().read_line(&mut input).unwrap();
+        input.pop();
+        let command: Vec<&str> = input.split(" ").collect();
+        if command[0] == "exit" {
+            break;
+        }
+        eval_command(command[0], command[1..command.len()].to_vec());
+    }
+}
 
-        let mut command = String::new();
-        io::stdin().read_line(&mut command).unwrap();
-        let command = command.trim();
-
-        let (name, arg) = command
-            .split_once(' ')
-            .map(|(n, a)| (n, a.trim()))
-            .unwrap_or((command, ""));
-
-        match name {
-            "exit" => break,
-            "echo" => println!("{}", arg),
-            "type" => match VALID_COMMANDS.contains(&arg) {
-                true => println!("{} is a shell builtin", arg),
-                false => println!("{}: not found", arg),
-            },
-            _ => println!("{}: command not found", name),
+fn eval_command(command: &str, args: Vec<&str>) {
+    let known_commands = ["exit", "echo", "type"];
+    if !known_commands.contains(&command) {
+        println!("{}: command not found", command);
+        return;
+    }
+    if command == "echo" {
+        for arg in &args {
+            print!("{} ", arg);
+        }
+        println!();
+        return;
+    }
+    if command == "type" {
+        if known_commands.contains(&args[0]) {
+            println!("{} is a shell builtin", args[0]);
+        } else if let Some(path) = find_executable_in_path(args[0]) {
+            println!("{} is {}", args[0], path.display());
+        } else {
+            println!("{}: not found", args[0]);
         }
     }
 }

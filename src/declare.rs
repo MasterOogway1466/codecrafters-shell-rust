f42686085619ecb91a1940cbb81682be9767cd63
MasterOogway1466 <manas.shetty04@gmail.com>
2026-05-08 10:54:45 +0530

    codecrafters submit [skip ci]

--- src/declare.rs
+++ src/declare.rs
@@ -11,9 +11,16 @@ pub fn handle_declare(args: &[String]) {
             if let Some(name) = args.get(1) {
                 VARIABLES.with(|v| {
                     match v.borrow().get(name.as_str()) {
-                        Some(val) => println!("declare -x {}=\"{}\"", name, val),
+                        Some(val) => println!("declare -- {}=\"{}\"", name, val),
                         None => eprintln!("declare: {}: not found", name),
                     }
+                });
+            }
+        }
+        Some(assignment) if assignment.contains('=') => {
+            if let Some((name, value)) = assignment.split_once('=') {
+                VARIABLES.with(|v| {
+                    v.borrow_mut().insert(name.to_string(), value.to_string());
                 });
             }
         }
